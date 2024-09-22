086d87c881273bffb6179ca145db8c0e6bac0168
AzarBinaPardazesh <a.rahmaniazar@gmail.com>
2024-09-22 12:30:53 +0330

    Remove all files except .gitignore and README.md

--- .github/Parnas/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Parnas.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
